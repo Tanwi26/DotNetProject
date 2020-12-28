8f9cb9f6d965a67d0e5addada7f1df29574ec63d
Tanwi J <tanwijakhadi26@gmail.com>
2020-12-28 22:53:42 +0530

    Initial commit

---
+++ DotNetProject/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name  { get; set; }
+        [Required]
+        public string DOB { get; set; }
+        [Required]
+        [EmailAddress]
+       
+        public string Email { get; set; }
+        [Required]
+        [StringLength(10,MinimumLength =10)]
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        public string City { get; set; }
+    }
+}
